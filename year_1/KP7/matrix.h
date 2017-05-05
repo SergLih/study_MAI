e3464746e87b38b16101a38bc722a1489203a779
SergLih <lixarev.s@mail.ru>
2017-05-05 17:40:16 +0300

    Добавлены функции row_create и row_destroy

--- year_1/KP7/matrix.h
+++ year_1/KP7/matrix.h
@@ -1,11 +1,10 @@
-#ifndef __MATRIX_H__
-#define __MATRIX_H__
-
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
 #include <complex.h>
 
-typedef double complex elem_type;
+typedef complex double elem_type;
 
-typedef struct matrix {
+typedef struct {
     int n;
     int m;
     int entries;
@@ -18,15 +17,16 @@ typedef struct {
     int n;
     elem_type *elem;
 } Row;
-    
-void create(Matrix *mat, int n, int m);
-void destruction(Matrix *mat);
-elem_type get(Matrix *mat, int i, int j);
-void set(Matrix *mat, int i, int j, elem_type value);
+
+void row_create(Row *row, int n);
+void row_destroy(Row *row);
+void matrix_create(Matrix *mat, int n, int m);
+void matrix_destroy(Matrix *mat);
+void matrix_set(Matrix *mat, int i, int j, elem_type val);
+elem_type matrix_get(Matrix *mat, int i, int j);
 void multrow(Row *row, Matrix *mat, Matrix *result);
 void multsparse(Matrix *mat1, Matrix *mat2, Matrix *result);
-void print(Matrix *mat);
-void printsparse(Matrix *mat);
+void print_full_matrix(Matrix *mat);
+void print_in_computer_view(Matrix *mat);
 int nonzero(Matrix *mat);
-
-#endif // __MATRIX_H__
+#endif 
