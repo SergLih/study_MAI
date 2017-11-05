41816389a87ca022f8ad61c8d2fa52b1313ddd4c
SergLih <lixarev.s@mail.ru>
2017-11-06 01:28:20 +0300

    add class of pentagon

---
+++ year_2/OOP/lab4/pentagon.h
@@ -0,0 +1,25 @@
+#ifndef PENTAGON_H
+#define PENTAGON_H
+#include <cstdlib>
+#include <iostream>
+#include "figure.h"
+
+class Pentagon : public Figure {
+	public:
+	    Pentagon();
+		Pentagon(size_t side);
+		Pentagon(std::istream &is);
+		Pentagon(const Pentagon& orig);
+		double Square() override;
+        
+        friend std::ostream& operator<<(std::ostream& os, const Pentagon& obj);
+        friend std::istream& operator>>(std::istream& is,  Pentagon& obj);
+		void print(std::ostream& os) const;
+        Pentagon& operator=(const Pentagon& right);
+		
+		virtual ~Pentagon();
+	private:
+		int side;
+};
+
+#endif   /*  Pentagon_H  */
