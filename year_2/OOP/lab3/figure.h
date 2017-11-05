03729c11c158fc67dbf895926f7cd76485f4baa6
SergLih <lixarev.s@mail.ru>
2017-11-05 16:59:15 +0300

    add figure.h

--- year_2/OOP/lab3/figure.h
+++ year_2/OOP/lab3/figure.h
@@ -1,10 +1,19 @@
 #ifndef FIGURE_H
 #define FIGURE_H
 
+#include <iostream>
+#include <memory>
+//#define DEBUG 0
+
 class Figure {
 	public:
+        virtual void print(std::ostream& os) const;
 		virtual double Square() = 0;
+		bool TypedEquals(std::shared_ptr<Figure> other);
+		bool SquareLess(std::shared_ptr<Figure> other);
+		bool SquareGreater(std::shared_ptr<Figure> other);
 		virtual ~Figure() {};
 };
+	
 
 #endif  /*  FIGURE_H  */
