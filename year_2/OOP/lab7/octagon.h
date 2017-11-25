6475ca464ad32770d51e9bc98685353d36f238d7
SergLih <lixarev.s@mail.ru>
2017-11-25 20:48:07 +0300

    create class of octagon

---
+++ year_2/OOP/lab7/octagon.h
@@ -0,0 +1,25 @@
+#ifndef OCTAGON_H
+#define OCTAGON_H
+#include <cstdlib>
+#include <iostream>
+#include "figure.h"
+
+class Octagon : public Figure {
+public:
+	Octagon();
+	Octagon(size_t side);
+	Octagon(std::istream &is);
+	Octagon(const Octagon& orig);
+	double Square() override;
+  
+  friend std::ostream& operator<<(std::ostream& os, const Octagon& obj);
+	friend std::istream& operator>>(std::istream& is, Octagon& obj);
+	void print(std::ostream& os) const;
+	Octagon& operator=(const Octagon& right);
+
+	virtual ~Octagon();
+private:
+	int side;
+};
+
+#endif   /*  Octagon_H  */
