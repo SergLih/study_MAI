6e1c36ab953ab706b275ad2dca0aa0f9f24cd765
SergLih <lixarev.s@mail.ru>
2017-11-25 11:54:07 +0300

    create binary tree item

---
+++ year_2/OOP/lab6/tbinary_tree_item.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+
+#include "tbinary_tree.h"
+
+//это инициализация, то есть присваивание начального значения статическому полю класса TreeNode.
+template <class T> TAllocationBlock	//происходит только один раз, так как это единое статическое поле для всех объектов класса
+TreeNode<T>::allocator(sizeof(TreeNode<T>), MAX_TREE_CAPACITY);
+
+template <class T>
+TreeNode<T>::TreeNode() {
+#ifdef DEBUG
+	std::cout << "TreeNode: default ctr ";
+#endif 
+	left = nullptr;
+	right = nullptr;
+	parent = nullptr;
+}
+
+template<class T>
+TreeNode<T>::TreeNode(std::shared_ptr<T> data, TreeNodePtr<T> parent)
+{
+	left = nullptr;
+	right = nullptr;
+	this->parent = parent;
+	this->data = data;
+}
+
+template<class T>
+std::shared_ptr<T> TreeNode<T>::GetPtr()
+{
+	return data;
+}
+
+template <class T>
+void *TreeNode<T>::operator new(size_t size)
+{
+	return allocator.Allocate();
+}
+
+template <class T>
+void TreeNode<T>::operator delete(void *ptr)
+{
+	allocator.Deallocate(ptr);
+}
+
+template class TreeNode<Figure>;
