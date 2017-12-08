8973f4de281f15bf10224dd9d151e7cb7c02ed3b
SergLih <lixarev.s@mail.ru>
2017-12-08 22:33:03 +0300

    add print for square

--- year_2/OOP/lab7/figure.cpp
+++ year_2/OOP/lab7/figure.cpp
@@ -5,17 +5,23 @@ void Figure::print(std::ostream & os) const
 {
 }
 
-bool Figure::TypedEquals(std::shared_ptr<Figure> other)
+bool Figure::TypedEquals(std::shared_ptr<Figure> other) const
 {
 	return (this->Square() == other->Square() && typeid(*this) == typeid(*other.get()));
 }
 
-bool Figure::SquareLess(std::shared_ptr<Figure> other)
+bool Figure::SquareLess(std::shared_ptr<Figure> other) const
 {
 	return (this->Square() < other->Square());
 }
 
-bool Figure::SquareGreater(std::shared_ptr<Figure> other)
+bool Figure::SquareGreater(std::shared_ptr<Figure> other) const
 {
 	return this->Square() > other->Square();
+}
+
+std::ostream & operator<<(std::ostream & os, const Figure & obj)
+{
+	obj.print(os);
+	return os;
 }
