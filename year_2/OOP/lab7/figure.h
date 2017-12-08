b5d409cab45235621e8cab191f329c88f4a3ea74
SergLih <lixarev.s@mail.ru>
2017-12-08 22:34:06 +0300

    add operator <<

--- year_2/OOP/lab7/figure.h
+++ year_2/OOP/lab7/figure.h
@@ -8,11 +8,13 @@
 class Figure {
 	public:
 		virtual void print(std::ostream& os) const = 0;
-		virtual double Square() = 0;
-		bool TypedEquals(std::shared_ptr<Figure> other);
-		bool SquareLess(std::shared_ptr<Figure> other);
-		bool SquareGreater(std::shared_ptr<Figure> other);
+		virtual double Square() const = 0;
+		bool TypedEquals(std::shared_ptr<Figure> other) const;
+		bool SquareLess(std::shared_ptr<Figure> other) const;
+		bool SquareGreater(std::shared_ptr<Figure> other) const;
 		virtual ~Figure() {};
+
+		friend std::ostream& operator<<(std::ostream& os, const Figure& obj);
 };
 
 #endif  /*  FIGURE_H  */
